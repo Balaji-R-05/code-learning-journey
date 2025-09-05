f4b6f19ee6808774aae2f29ed2f25d877333199b
Balaji-R-05 <balajiramu23@gmail.com>
2025-09-05 23:36:38 +0530

    renamed shell to bash

--- Rust/hello-world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-O:\code-learning-journey\Rust\hello-world\target\debug\deps\hello_world.exe: src/main.rs
-
-O:\code-learning-journey\Rust\hello-world\target\debug\deps\hello_world.d: src/main.rs
-
-src/main.rs:
--- Rust/hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-O:\code-learning-journey\Rust\hello-world\target\debug\hello-world.exe: O:\code-learning-journey\Rust\hello-world\src\main.rs
